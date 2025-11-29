9260fcef7c2f74c61a2660fce1e66b37032090fb
Zaffer Hussein <zafferhussein1@gmail.com>
2025-11-28 23:53:20 -0500

    quick start guide

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,62 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world!");
+#[derive(Component)]
+struct Car {
+    color: String,
+    position: Position,
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Resource)]
+struct HonkTimer(Timer);
+
+fn create_cars(mut commands: Commands) {
+    commands.spawn(Car {
+        color: "red".to_string(),
+        position: Position { x: (0), y: (0) },
+    });
+    commands.spawn(Car {
+        color: "green".to_string(),
+        position: Position { x: (10), y: (10) },
+    });
+}
+
+/*fn reset_cars(mut query: Query<&mut Car>) {
+    for mut car in &mut query {
+        car.position.x = 0;
+        car.position.y = 0;
+    }
+}*/
+
+fn honk_cars(time: Res<Time>, mut timer: ResMut<HonkTimer>, query: Query<&Car>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for car in &query {
+            println!(
+                "a {} car at location ({},{}) honks!",
+                car.color, car.position.x, car.position.y
+            );
+        }
+    }
 }
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(CarPlugin)
+        .run();
+}
+
+pub struct CarPlugin;
+
+impl Plugin for CarPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(HonkTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, create_cars);
+        app.add_systems(Update, honk_cars);
+    }
 }
